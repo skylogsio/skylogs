3273b344d50b52c2fa825a75946e77ce00c50395
moheb79 <mhmdhbhadr@gmail.com>
2024-12-26 17:23:46 +0330

    feat: ✨Make user CreateModal useable for CRU

--- apps/frontend/src/app/[locale]/users/CreateModal.tsx
+++ apps/frontend/src/app/[locale]/users/CreateModal.tsx
@@ -15,7 +15,13 @@ import ToggleButtonGroup from "@/components/ToggleButtonGroup";
 
 import { CreateUserModalProps } from "./types";
 
-export default function CreateUserModal({ open, onClose }: CreateUserModalProps) {
+const BUTTON_TEXT: { [K in NonNullable<CreateUserModalProps["mode"]>]: string } = {
+  CREATE: "Create",
+  UPDATE_INFO: "Edit",
+  UPDATE_PASSWORD: "Change Password"
+};
+
+export default function CreateUserModal({ open, onClose, mode = "CREATE" }: CreateUserModalProps) {
   const [showPassword, setShowPassword] = useState(false);
 
   function handleTogglePassword(event: ChangeEvent<HTMLInputElement>, checked: boolean) {
@@ -29,40 +35,51 @@ export default function CreateUserModal({ open, onClose }: CreateUserModalProps)
           <Typography variant="body1" component="div" marginRight="0.7rem">
             Role:
           </Typography>
-          <ToggleButtonGroup value="user">
+          <ToggleButtonGroup value="user" disabled={mode === "UPDATE_PASSWORD"}>
             <ToggleButton value="user">User</ToggleButton>
             <ToggleButton value="admin">Admin</ToggleButton>
           </ToggleButtonGroup>
         </Grid>
         <Grid size={6}>
-          <TextField label="Username" variant="filled" />
+          <TextField label="Username" variant="filled" disabled={mode === "UPDATE_PASSWORD"} />
         </Grid>
         <Grid size={6}>
-          <TextField label="Full Name" variant="filled" />
+          <TextField label="Full Name" variant="filled" disabled={mode === "UPDATE_PASSWORD"} />
         </Grid>
-        <Grid size={6}>
-          <TextField label="Password" type={showPassword ? "text" : "password"} variant="filled" />
-        </Grid>
-        <Grid size={6}>
-          <TextField
-            label="Confirm Password"
-            type={showPassword ? "text" : "password"}
-            variant="filled"
-          />
-        </Grid>
-        <Grid size={6} display="flex">
-          <FormControlLabel
-            sx={{ userSelect: "none" }}
-            control={
-              <Checkbox id="ShowPassword" checked={showPassword} onChange={handleTogglePassword} />
-            }
-            label="Show Password"
-          />
-        </Grid>
-
+        {mode !== "UPDATE_INFO" && (
+          <>
+            <Grid size={6}>
+              <TextField
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                variant="filled"
+              />
+            </Grid>
+            <Grid size={6}>
+              <TextField
+                label="Confirm Password"
+                type={showPassword ? "text" : "password"}
+                variant="filled"
+              />
+            </Grid>
+            <Grid size={6} display="flex">
+              <FormControlLabel
+                sx={{ userSelect: "none" }}
+                control={
+                  <Checkbox
+                    id="ShowPassword"
+                    checked={showPassword}
+                    onChange={handleTogglePassword}
+                  />
+                }
+                label="Show Password"
+              />
+            </Grid>
+          </>
+        )}
         <Grid size={12} marginTop="1rem">
           <Button variant="contained" size="large" fullWidth>
-            Create
+            {BUTTON_TEXT[mode]}
           </Button>
         </Grid>
       </Grid>
