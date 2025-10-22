3e1db31f45cf6e50373cc3b94fae85fa4ea21693
M <amirmasoud.asadi@gmail.com>
2025-10-22 22:09:15 +0330

    update: alert rule description

---
+++ apps/backend/app/Docs/GeneralDocs.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Docs;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: '1.0.0',
+    description: 'Skylogs backend API documentation',
+    title: 'Skylogs API'
+)]
+#[OA\Server(
+    url: 'http://localhost:9000',
+    description: 'Skylogs local server'
+)]
+#[OA\Components(
+    securitySchemes: [
+        new OA\SecurityScheme(
+            securityScheme: 'bearerAuth',
+            type: 'http',
+            description: 'Enter JWT token (without “Bearer” prefix)',
+            bearerFormat: 'JWT',
+            scheme: 'bearer'
+        ),
+    ]
+)]
+#[OA\SecurityRequirement(name: 'bearerAuth')]
+class GeneralDocs {}
