bc901dcf2a1e8c58089ab1a7bd076df20676e667
moheb79 <mhmdhbhadr@gmail.com>
2025-10-31 12:22:35 +0330

    feat: 👽️Change the structure of telegram proxies

--- apps/frontend/src/@types/settings/telegram.ts
+++ apps/frontend/src/@types/settings/telegram.ts
@@ -1,8 +1,12 @@
-export interface ITelegramProxy{
-  id:string;
-  name:string;
-  url:string;
-  active:boolean;
-  updatedAt:string;
-  createdAt:string;
+export interface ITelegramProxy {
+  id: string;
+  name: string;
+  active: boolean;
+  updatedAt: string;
+  createdAt: string;
+  type: "http" | "socks5";
+  host: string;
+  port: number;
+  username: string;
+  password: string;
 }
