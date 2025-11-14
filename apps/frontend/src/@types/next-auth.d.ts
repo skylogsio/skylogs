78630c4c31287c26ef37854792f988011fca9add
mhmdhbhadr <mhmdhbhadr@gmail.com>
2025-11-14 12:08:32 +0330

    fix(WIP): Work on signing out after token expiration

--- apps/frontend/src/@types/next-auth.d.ts
+++ apps/frontend/src/@types/next-auth.d.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-import NextAuth, { DefaultSession, type Session, type Account } from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
