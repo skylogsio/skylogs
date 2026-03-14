ecc42d2885397aa8b456197d4ffecdf64a0e789d
M <amirmasoud.asadi@gmail.com>
2026-03-14 22:18:33 +0330

    fix: api route

--- apps/backend/routes/api.php
+++ apps/backend/routes/api.php
@@ -78,7 +78,7 @@ Route::prefix('v1')->group(function () {
                 Route::middleware('role:'.Constants::ROLE_OWNER->value)->post('/changeOwner', 'ChangeOwnerShipOfData');
                 Route::middleware('role:'.Constants::ROLE_OWNER->value.'|'.Constants::ROLE_MANAGER->value)->group(function () {
                     Route::get('/', 'Index');
-                    Route::get('/{id}', 'Show');
+                    Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                     Route::post('/', 'Create');
                     Route::put('/pass/{id}', 'ChangePassword');
                     Route::put('/{id}', 'Update');
@@ -93,7 +93,7 @@ Route::prefix('v1')->group(function () {
                 Route::get('/', 'Index');
                 Route::get('/indexFlow', 'IndexFlow');
                 Route::get('/createFlowEndpoints', 'EndpointsToCreateFlow');
-                Route::get('/{id}', 'Show');
+                Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                 Route::post('/', 'Create');
                 Route::post('/sendOTP', 'SendOTPCode');
                 Route::put('/{id}', 'Update');
@@ -107,7 +107,7 @@ Route::prefix('v1')->group(function () {
 
                 Route::middleware('role:'.Constants::ROLE_OWNER->value)->group(function () {
                     Route::get('/', 'Index');
-                    Route::get('/{id}', 'Show');
+                    Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                     Route::post('/', 'Create');
                     Route::put('/{id}', 'Update');
                     Route::delete('/{id}', 'Delete');
@@ -124,7 +124,7 @@ Route::prefix('v1')->group(function () {
             ->group(function () {
                 Route::get('/', 'Index');
                 Route::get('/status/{id}', 'IsConnected');
-                Route::get('/{id}', 'Show');
+                Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                 Route::get('/types', 'GetTypes');
                 Route::post('/', 'Create');
                 Route::put('/{id}', 'Update');
@@ -136,7 +136,7 @@ Route::prefix('v1')->group(function () {
             ->group(function () {
                 Route::get('/', 'Index');
                 Route::get('/all', 'All');
-                Route::get('/{id}', 'Show');
+                Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                 Route::middleware('role:'.Constants::ROLE_OWNER->value.'|'.Constants::ROLE_MANAGER->value)->post('/', 'Create');
                 Route::middleware('role:'.Constants::ROLE_OWNER->value.'|'.Constants::ROLE_MANAGER->value)->put('/{id}', 'Update');
                 Route::middleware('role:'.Constants::ROLE_OWNER->value.'|'.Constants::ROLE_MANAGER->value)->delete('/{id}', 'Delete');
@@ -147,7 +147,7 @@ Route::prefix('v1')->group(function () {
             ->middleware('role:'.Constants::ROLE_OWNER->value.'|'.Constants::ROLE_MANAGER->value)
             ->group(function () {
                 Route::get('/', 'Index');
-                Route::get('/{id}', 'Show');
+                Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                 Route::post('/', 'Create');
                 Route::put('/{id}', 'Update');
                 Route::delete('/{id}', 'Delete');
@@ -182,7 +182,7 @@ Route::prefix('v1')->group(function () {
                         Route::post('/add-user-notify', 'AddUserAccessNotify');
                     });
 
-                Route::get('/{id}', 'Show');
+                Route::get('/{id}', 'Show')->where('id', '[0-9a-fA-F]{24}');
                 Route::post('/', 'Store');
                 Route::post('/silent/{id}', 'Silent');
                 Route::post('/pin/{id}', 'Pin');
