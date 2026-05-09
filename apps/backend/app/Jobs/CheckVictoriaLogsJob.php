31505911aa54f0f925afe58bdcdf698c999c0d0e
M <amirmasoud.asadi@gmail.com>
2026-05-09 19:52:12 +0330

    fix: victoria log notify type

--- apps/backend/app/Jobs/CheckVictoriaLogsJob.php
+++ apps/backend/app/Jobs/CheckVictoriaLogsJob.php
@@ -76,7 +76,7 @@ class CheckVictoriaLogsJob implements ShouldQueue
                     'state' => VictoriaLogsCheck::FIRE,
                 ]);
 
-                SendNotifyService::CreateNotify(SendNotifyJob::ELASTIC_CHECK, $check, $this->alert->_id);
+                SendNotifyService::CreateNotify(SendNotifyJob::VICTORIA_LOGS_CHECK, $check, $this->alert->_id);
             } elseif ($check->currentCountDocument !== $countDocuments) {
                 $check->currentCountDocument = $countDocuments;
                 $check->save();
