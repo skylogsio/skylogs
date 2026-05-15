db598f5dbb5829d4987321742968d29ce9def3b9
moheb79 <mhmdhbhadr@gmail.com>
2026-05-15 11:14:57 +0330

    chore: Upgrade NextJS and ReactJS to latest version

--- apps/frontend/src/app/[locale]/layout.tsx
+++ apps/frontend/src/app/[locale]/layout.tsx
@@ -36,11 +36,12 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params: { locale }
-}: PropsWithChildren<{ params: { locale: string } }>) {
+  params
+}: PropsWithChildren<{ params: Promise<{ locale: string }> }>) {
+  const { locale } = await params;
   const dir = await getCurrentDirection();
   const cookieStore = cookies();
-  const theme = cookieStore.get("theme");
+  const theme = (await cookieStore).get("theme")?.value;
 
   return (
     <html lang={locale} dir={dir}>
